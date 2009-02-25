2046e98bc897ed60fc5033f48758bba192177ad0
Ryan Neufeld <neufelry@gmail.com>
2009-02-25 12:17:35 -0600

    Updated core, still working on plugin installs

--- core.rb
+++ core.rb
@@ -1,10 +1,14 @@
 load_template "http://github.com/53cr/templates/raw/master/base.rb"
 
 # Load Core
+git :clone => "git@github.com:53cr/core.git vendor/plugins/core"#, :checkout => "edge origin/edge"
 
 # "Coreify"
-run "rsync -ruv vendor/plugins/core/db/migrate db"
-run "rsync -ruv vendor/plugins/core/public ."
-run "rsync -ruv vendor/plugins/core/config/initializers config/"
-run "rsync -ruv vendor/plugins/core/vendor/plugins vendor/plugins"
-run "rsync -ruv vendor/plugins/core/app/views/user_mailer app/views/"
+system "rsync -ruv vendor/plugins/core/db/migrate db" # required for now
+system "rsync -ruv vendor/plugins/core/public ." # possibly only copy curtain things
+system "rsync -ruv vendor/plugins/core/config/initializers config/" # hack this should turn into configeration tools for each engine
+Dir.glob("vendor/plugins/core/vendor/plugins/**").each do |plugin|
+  # Doesn't work
+  system "./script/plugin install #{plugin}/"
+end
+system "rsync -ruv vendor/plugins/core/app/views/user_mailer app/views/"
